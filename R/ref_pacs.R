a6f24d2e43628036787103f6f029d65822606472
dswatson <david.s.watson11@gmail.com>
2017-08-02 02:09:46 +0200

    svd

--- R/ref_pacs.R
+++ R/ref_pacs.R
@@ -5,24 +5,24 @@
 #'
 #' @param dat Probe by sample omic data matrix. Data should be filtered and
 #'   normalized prior to analysis.
-#' @param maxK Maximum cluster number to evaluate.
-#' @param refMethod How should null data be generated? Options include \code{
-#'   reverse-pca}, \code{cholesky}, \code{range}, and \code{permute}.
+#' @param max_k Maximum cluster number to evaluate.
+#' @param ref_method How should null data be generated? Options include \code{
+#'   svd}, \code{cholesky}, \code{range}, and \code{permute}.
 #' @param B Number of null datasets to generate.
 #' @param reps Number of subsamples to draw for consensus clustering.
 #' @param distance Distance metric for clustering. Supports all methods
 #'   available in \code{\link[stats]{dist}} and \code{\link[vegan]{vegdist}},
 #'   as well as those implemented in the \code{bioDist} package.
-#' @param clusterAlg Clustering algorithm to implement. Currently supports
+#' @param cluster_alg Clustering algorithm to implement. Currently supports
 #'   hierarchical (\code{"hclust"}), \emph{k}-means (\code{"kmeans"}), and
 #'   \emph{k}-medoids (\code{"pam"}).
-#' @param innerLinkage Method to use if \code{clusterAlg = "hclust"}. See \code{
+#' @param hclust_method Method to use if \code{cluster_alg = "hclust"}. See \code{
 #'   \link[stats]{hclust}}.
-#' @param pItem Proportion of items to include in each subsample.
-#' @param pFeature Proportion of features to include in each subsample.
-#' @param weightsItem Optional vector of item weights.
-#' @param weightsFeature Optional vector of feature weights.
-#' @param pacWindow Lower and upper bounds for the consensus index sub-interval
+#' @param p_item Proportion of items to include in each subsample.
+#' @param p_feature Proportion of features to include in each subsample.
+#' @param wts_item Optional vector of item weights.
+#' @param wts_feature Optional vector of feature weights.
+#' @param pac_window Lower and upper bounds for the consensus index sub-interval
 #'   over which to calculate the PAC. Must be on (0, 1).
 #' @param seed Optional seed for reproducibility.
 #' @param parallel If a parallel backend is loaded and available, should the 
@@ -38,34 +38,33 @@
 #' 
 #' Just as reference PAC distributions are the theoretical core of the M3C 
 #' approach to cluster validation, \code{ref_pacs} is the computational core 
-#' of the \code{M3C} package. This function can take some time to execute, and 
-#' should ideally be run in parallel, especially with large datasets. 
+#' of the \code{CCtestr} package. This function can take some time to execute, 
+#' and should ideally be run in parallel, especially with large datasets. 
 #' 
-#' @return A matrix with \code{B} rows and \code{maxK - 1} columns containing 
+#' @return A matrix with \code{B} rows and \code{max_k - 1} columns containing 
 #' null PAC scores for each cluster number \emph{k}.
 #'
 #' @examples
 #' mat <- matrix(rnorm(1000 * 12), nrow = 1000, ncol = 12)
-#' pca <- prcomp(t(mat))
-#' rp <- ref_pacs(mat, pca = pca, refMethod = "reverse-pca")
+#' rp <- ref_pacs(mat, ref_method = "svd")
 #'
 #' @export
 #' @importFrom matrixStats colSds
 #' 
 
 ref_pacs <- function(dat, 
-                     maxK = 3, 
-                     refMethod = 'reverse-pca',
+                     max_k = 3, 
+                     ref_method = 'svd',
                      B = 100,
                      reps = 100, 
                      distance = 'euclidean', 
-                     clusterAlg = 'hclust', 
-                     innerLinkage = 'average',
-                     pItem = 0.8, 
-                     pFeature = 1, 
-                     weightsItem = NULL, 
-                     weightsFeature = NULL, 
-                     pacWindow = c(0.1, 0.9), 
+                     cluster_alg = 'hclust', 
+                     hclust_method = 'average',
+                     p_item = 0.8, 
+                     p_feature = 1, 
+                     wts_item = NULL, 
+                     wts_feature = NULL, 
+                     pac_window = c(0.1, 0.9), 
                      seed = NULL,
                      parallel = TRUE) {
   
@@ -77,52 +76,52 @@ ref_pacs <- function(dat,
     dat <- exprs(dat)
   }
   dat <- as.matrix(dat)
-  sample_n <- round(pItem * ncol(dat))
-  if (maxK > sample_n) {
-    stop('maxK exceeds subsample size.')
+  sample_n <- round(p_item * ncol(dat))
+  if (max_k > sample_n) {
+    stop('max_k exceeds subsample size.')
   }
-  if (!refMethod %in% c('reverse-pca', 'cholesky', 'range', 'permute')) {
-    stop('refMethod must be one of "reverse-pca", "cholesky", "range", or ',
+  if (!ref_method %in% c('svd', 'cholesky', 'range', 'permute')) {
+    stop('ref_method must be one of "svd", "cholesky", "range", or ',
          '"permute".')
   }
-  if (!innerLinkage %in% c('ward.D', 'ward.D2', 'single', 'complete',
+  if (!hclust_method %in% c('ward.D', 'ward.D2', 'single', 'complete',
                            'average', 'mcquitty', 'median', 'centroid')) {
-    stop('innerLinkage must be one of "ward.D", "ward.D2", "single", ',
+    stop('hclust_method must be one of "ward.D", "ward.D2", "single", ',
          '"complete", "average" "mcquitty" "median" or "centroid". See ?hclust.')
   }
-  if (pItem > 1 || pItem <= 0) {
-    stop('pItem must be on (0, 1].')
+  if (p_item > 1 || p_item <= 0) {
+    stop('p_item must be on (0, 1].')
   }
-  if (pFeature > 1 || pFeature <= 0) {
-    stop('pFeature must be on (0, 1].')
+  if (p_feature > 1 || p_feature <= 0) {
+    stop('p_feature must be on (0, 1].')
   }
-  if (!is.null(weightsItem)) {
-    if (length(weightsItem) != n) {
-      stop('weightsItem must a vector of length ncol(dat).')
+  if (!is.null(wts_item)) {
+    if (length(wts_item) != n) {
+      stop('wts_item must a vector of length ncol(dat).')
     }
-    if (max(weightsItem) > 1 || min(weightsItem) < 0) {
-      stop('All values in weightsItem must be on [0, 1].')
-    }
-  }
-  if (!is.null(weightsFeature)) {
-    if (length(weightsFeature) != p) {
-      stop('weightsItem must a vector of length nrow(dat).')
-    }
-    if (max(weightsFeature) > 1 || min(weightsFeature) < 0) {
-      stop('All values in weightsFeature must be on [0, 1].')
+    if (max(wts_item) > 1 || min(wts_item) < 0) {
+      stop('All values in wts_item must be on [0, 1].')
     }
   }
-  if (length(pacWindow) != 2) {
-    stop('pacWindow must be a vector of length 2.')
+  if (!is.null(wts_feature)) {
+    if (length(wts_feature) != p) {
+      stop('wts_item must a vector of length nrow(dat).')
+    }
+    if (max(wts_feature) > 1 || min(wts_feature) < 0) {
+      stop('All values in wts_feature must be on [0, 1].')
+    }
   }
-  if (min(pacWindow) <= 0 || max(pacWindow) >= 1) {
-    stop('Both values of pacWindow must be on (0, 1).')
+  if (length(pac_window) != 2) {
+    stop('pac_window must be a vector of length 2.')
+  }
+  if (min(pac_window) <= 0 || max(pac_window) >= 1) {
+    stop('Both values of pac_window must be on (0, 1).')
   }
   
   # Define pacs_b function
-  pacs_b <- function(b, dat, maxK, refMethod, B, reps, distance, 
-                     clusterAlg, innerLinkage, pItem, pFeature, weightsItem, 
-                     weightsFeature, pacWindow, seed) {
+  pacs_b <- function(b, dat, max_k, ref_method, B, reps, distance, 
+                     cluster_alg, hclust_method, p_item, p_feature, wts_item, 
+                     wts_feature, pac_window, seed) {
     n <- ncol(dat)
     p <- nrow(dat)
     # Set seed
@@ -130,8 +129,8 @@ ref_pacs <- function(dat,
       seed <- seed + b
     }
     # Generate null data
-    null_dat <- switch(refMethod, 
-      'reverse-pca' = {
+    null_dat <- switch(ref_method, 
+      'svd' = {
         pca <- prcomp(t(dat))
         sim_dat <- matrix(rnorm(n^2L, mean = 0L, sd = colSds(pca$x)),
                           nrow = n, ncol = n, byrow = TRUE)
@@ -152,37 +151,37 @@ ref_pacs <- function(dat,
         null_dat
       })
     # Generate consensus matrices
-    cm <- consensus(null_dat, maxK = maxK, reps = reps, distance = distance,
-                    clusterAlg = clusterAlg, innerLinkage = innerLinkage,
-                    pItem = pItem, pFeature = pFeature,
-                    weightsItem = weightsItem, weightsFeature = weightsFeature,
+    cm <- consensus(null_dat, max_k = max_k, reps = reps, distance = distance,
+                    cluster_alg = cluster_alg, hclust_method = hclust_method,
+                    p_item = p_item, p_feature = p_feature,
+                    wts_item = wts_item, wts_feature = wts_feature,
                     seed = seed, parallel = FALSE, check = FALSE)
     # Calculate PAC
-    pacs <- PAC(cm, pacWindow)$PAC
+    pacs <- PAC(cm, pac_window)$PAC
     return(pacs)
   }
   
   if (parallel) {
     # Execute in parallel
     null_pacs <- foreach(b = seq_len(B), .combine = rbind) %dopar%
-      pacs_b(b, dat = dat, maxK = maxK, pca = pca, cd = cd, 
-             refMethod = refMethod, B = B, reps = reps, distance = distance, 
-             clusterAlg = clusterAlg, innerLinkage = innerLinkage, 
-             pItem = pItem, pFeature = pFeature, 
-             weightsItem = weightsItem, weightsFeature = weightsFeature, 
-             pacWindow = pacWindow, seed = seed)
+      pacs_b(b, dat = dat, max_k = max_k, pca = pca, cd = cd, 
+             ref_method = ref_method, B = B, reps = reps, distance = distance, 
+             cluster_alg = cluster_alg, hclust_method = hclust_method, 
+             p_item = p_item, p_feature = p_feature, 
+             wts_item = wts_item, wts_feature = wts_feature, 
+             pac_window = pac_window, seed = seed)
   } else {
     # Execute in serial
     null_pacs <- t(sapply(seq_len(B), function(b) {
-      pacs_b(b, dat = dat, maxK = maxK, pca = pca, cd = cd, 
-             refMethod = refMethod, B = B, reps = reps, distance = distance, 
-             clusterAlg = clusterAlg, innerLinkage = innerLinkage, 
-             pItem = pItem, pFeature = pFeature, 
-             weightsItem = weightsItem, weightsFeature = weightsFeature, 
-             pacWindow = pacWindow, seed = seed)
+      pacs_b(b, dat = dat, max_k = max_k, pca = pca, cd = cd, 
+             ref_method = ref_method, B = B, reps = reps, distance = distance, 
+             cluster_alg = cluster_alg, hclust_method = hclust_method, 
+             p_item = p_item, p_feature = p_feature, 
+             wts_item = wts_item, wts_feature = wts_feature, 
+             pac_window = pac_window, seed = seed)
     }))
   }
-  dimnames(null_pacs) <- list(paste0('b', seq_len(B)), paste0('k', 2:maxK))
+  dimnames(null_pacs) <- list(paste0('b', seq_len(B)), paste0('k', 2:max_k))
   return(null_pacs)
     
 }
