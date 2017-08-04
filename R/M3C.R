a59cd5e54c28e94a9a9340d2e585d87b54b3c8d0
dswatson <david.s.watson11@gmail.com>
2017-08-04 20:08:34 +0200

    ref_method touchup

--- R/M3C.R
+++ R/M3C.R
@@ -4,14 +4,16 @@
 #'
 #' @param dat Probe by sample omic data matrix. Data should be filtered and
 #'   normalized prior to analysis.
-#' @param max_k Maximum cluster number to evaluate.
+#' @param max_k Integer specifying the maximum cluster number to evaluate. 
+#'   Default is \code{max_k = 3}, but a more reasonable rule of thumb is the 
+#'   square root of the sample size.
 #' @param null Generate null distribution for statistical comparison? If
 #'   \code{FALSE}, function implements the traditional consensus cluster
 #'   algorithm.
 #' @param ref_method How should null data be generated? Options include \code{
-#'   "pc_norm"}, \code{"pc_unif"}, \code{"cholesky"}, \code{range}, and \code{
+#'   pc_norm}, \code{pc_unif}, \code{cholesky}, \code{range}, and \code{
 #'   permute}. See Details.
-#' @param B Number of null datasets to generate.
+#' @param B Number of reference datasets to generate.
 #' @param reps Number of subsamples to draw for consensus clustering.
 #' @param distance Distance metric for clustering. Supports all methods
 #'   available in \code{\link[stats]{dist}} and \code{\link[vegan]{vegdist}},
@@ -37,50 +39,51 @@
 #' @details
 #' M3C is a hypothesis testing framework for consensus clustering. It takes an
 #' input matrix \code{dat}, and generates \code{B} null datasets with similar
-#' properties but no samplewise cluster structure. The consensus cluster
+#' properties but no sample-wise cluster structure. The consensus cluster
 #' algorithm is then run on each simulated matrix, with PAC scores stored for
 #' reference. The function then consensus clusters the actual input data, and
 #' PAC scores for each cluster number \emph{k} are tested against their
 #' empirically estimated null distribution.
 #'
-#' \code{M3C} currently supports five methods for generating null datasets from
+#' \code{M3C} currently supports four methods for generating null datasets from
 #' a given input matrix: 
 #' 
 #' \itemize{
 #'   \item \code{"pc_norm"} simulates the principal components by taking random 
 #'     draws from a normal distribution with variance equal to the true 
-#'     eigenvalues. Data are subsequently backtransformed to their original 
+#'     eigenvalues. Data are subsequently back-transformed to their original 
 #'     dimensions by cross-multiplication with the true eigenvector matrix.
 #'   \item \code{"pc_unif"} simulates the principal components by taking random 
 #'     draws from a uniform distribution with ranges equal to those of the true 
-#'     principal components. Data are subsequently backtransformed to their 
+#'     principal components. Data are subsequently back-transformed to their 
 #'     original dimensions by cross-multiplication with the true eigenvector 
 #'     matrix. 
-#'   \item \code{"cholesky"} simulates random Gaussian noise around the Cholesky
-#'     decomposition of the feature-wise covariance matrix's nearest positive
-#'     definite approximation.
+#'   \item \code{"cholesky"} simulates random Gaussian noise around the nearest
+#'     positive-definite approximation to \code{dat}'s feature-wise covariance 
+#'     matrix.
 #'   \item \code{"range"} selects random values uniformly from each feature's 
 #'     observed range. 
-#'   \item \code{"permute"} shuffles the values of each feature.
+#'   \item \code{"permute"} shuffles each feature's observed values.
 #' }
 #' 
-#' The first three options use different forms of matrix decomposition to 
-#' preserve the feature-wise covariance while scrambling the item-wise 
-#' covariance. \code{"pc_norm"} tends to generate the most realistic null data, 
-#' while \code{"pc_unif"} is more likely to guarantee a true \emph{k} of 1. Both 
-#' methods are fast and stable when features outnumber items. When items 
-#' outnumber features, \code{ref_method} defaults to \code{"cholesky"}, which 
-#' takes longer to compute, but is better suited for such cases. The final two 
-#' options are included for convenience, but do not preserve featurewise 
-#' covariance, which may bias results. 
+#' The first two options use the data's true eigenvectors to preserve 
+#' feature-wise covariance while scrambling sample-wise covariance. \code{
+#' "pc_norm"} tends to generate the most realistic null data, while Monte Carlo 
+#' replicates generated via \code{"pc_unif"} converge more quickly to a true 
+#' \emph{k} of 1. Both methods are fast and stable when features outnumber 
+#' samples. When samples outnumber features, \code{ref_method} defaults to 
+#' \code{"cholesky"}, which takes longer to compute, but is better suited for 
+#' such cases. \code{"range"} and \code{"permute"} are included for convenience,
+#' but are not recommended since they do not preserve feature-wise covariance,
+#' which may bias results. 
 #'
 #' PAC stands for proportion of ambiguous clustering. To calculate the PAC for a
 #' given cluster number \emph{k}, we first compute the consensus matrix via
 #' consensus clustering; then generate the empirical CDF curve for the lower
 #' triangle of that matrix; find CDF-values for the upper and lower bounds of
-#' the PAC window; and subtract the latter number from the former. Since the
-#' consensus matrix for a perfectly stable cluster would consist of just 1s and
-#' 0s, the ideal CDF curve is flat in the middle. The goal is therefore to
+#' the PAC window; and subtract the latter value from the former. Since the
+#' consensus matrix for a perfectly stable cluster would consist of just 1's and
+#' 0's, the ideal CDF curve is flat in the middle. The goal is therefore to
 #' minimize the PAC. See Senbabaoglu et al. (2014) for more details.
 #'
 #' @return A list with \code{max_k} elements. If \code{null = TRUE}, then
@@ -176,8 +179,9 @@ M3C <- function(dat,
   if (null) {
     message('Simulating null distributions...')
     ref_pacs_mat <- ref_pacs(dat, max_k = max_k, ref_method = ref_method, B = B, 
-                             reps = reps, distance = distance, cluster_alg = cluster_alg, 
-                             hclust_method = hclust_method, p_item = p_item, p_feature = p_feature, 
+                             reps = reps, distance = distance, 
+                             cluster_alg = cluster_alg, hclust_method = hclust_method, 
+                             p_item = p_item, p_feature = p_feature, 
                              wts_item = wts_item, wts_feature = wts_feature, 
                              pac_window = pac_window, seed = seed, parallel = parallel)
     message('Finished simulating null distributions.')
